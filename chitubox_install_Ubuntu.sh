c47ce143e4f1b9cb97834f44bf4a092ca124dc16
Eric Dalongeville <22616866+edalongeville@users.noreply.github.com>
2020-10-13 07:50:59 +0200

    Version bump 1.7.0

--- chitubox_install_Ubuntu.sh
+++ chitubox_install_Ubuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script installs Chitubox on Ubuntu 20.04
 
-FILE="CHITUBOX_V1.6.3.tar.gz"
+FILE="CHITUBOX_V1.7.0.tar.gz"
 ICON="chitubox.png"
 DESKFILE="chitubox.desktop"
 INSTALL_DIR="/usr/bin"
